49a4d6738cb1012e414e9a9d2ceff4b63c4609aa
AndresHense <andreshense@gmail.com>
2022-05-29 17:26:52 -0300

    add filter funtionality

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -15,15 +15,17 @@ import axios from 'axios';
 import { Product, User } from '../types';
 import { getProducts, getUser } from '../api/api';
 
-const productss = [
+const emptyProducts: Product[] = [];
+emptyProducts.fill(
   {
-    _id: '1',
-    name: 'yamaha',
-    category: 'trash',
-    cost: 200,
+    _id: '',
+    name: '',
+    category: '',
+    cost: -1,
     img: { url: '', hdUrl: '' },
   },
-];
+  16
+);
 
 enum SortingType {
   MostRecent,
@@ -37,14 +39,32 @@ type Props = {
 };
 
 const ProductList = ({ user, setUser }: Props) => {
-  const [products, setProducts] = useState<Product[]>(productss);
+  const [products, setProducts] = useState<Product[]>(emptyProducts);
   const [loading, setLoading] = useState(true);
+  const [range, setRange] = useState({ start: 0, end: 16 });
+  const [showLeftArrow, setShowLeftArrow] = useState(false);
   const [sortingMethod, setSortingMethod] = useState<SortingType>(
     SortingType.MostRecent
   );
 
-  useEffect(() => {
+  const sortByHighestPrice = () => {
+    setProducts(products.sort((a, b) => b.cost - a.cost));
+  };
+  const sortByLowestPrice = () => {
+    setProducts(products.sort((a, b) => a.cost - b.cost));
+  };
+
+  const sortByMostRecent = () => {
     getProducts(setProducts);
+  };
+  useEffect(() => {
+    if (sortingMethod === SortingType.MostRecent) {
+      sortByMostRecent();
+    } else if (sortingMethod === SortingType.HighestPrice) {
+      sortByHighestPrice();
+    } else if (sortingMethod === SortingType.LowestPrice) {
+      sortByLowestPrice();
+    }
     setLoading(false);
   }, []);
 
@@ -53,7 +73,7 @@ const ProductList = ({ user, setUser }: Props) => {
       <HStack w='100%' justify='space-between' py={3}>
         <HStack spacing={6}>
           <Text fontSize='xl' color='gray.700'>
-            16 of 32 products
+            {range.end} of {products.length} products
           </Text>
           <Divider h='48px' borderWidth={1} orientation='vertical' />
           <Text fontSize='xl' color='gray.500'>
@@ -71,7 +91,10 @@ const ProductList = ({ user, setUser }: Props) => {
             color={
               sortingMethod === SortingType.MostRecent ? 'white' : 'gray.600'
             }
-            onClick={() => setSortingMethod(SortingType.MostRecent)}
+            onClick={() => {
+              setSortingMethod(SortingType.MostRecent);
+              sortByMostRecent();
+            }}
           >
             Most recent
           </Button>
@@ -87,7 +110,10 @@ const ProductList = ({ user, setUser }: Props) => {
             color={
               sortingMethod === SortingType.LowestPrice ? 'white' : 'gray.600'
             }
-            onClick={() => setSortingMethod(SortingType.LowestPrice)}
+            onClick={() => {
+              setSortingMethod(SortingType.LowestPrice);
+              sortByLowestPrice();
+            }}
           >
             Lowest Price
           </Button>
@@ -103,17 +129,42 @@ const ProductList = ({ user, setUser }: Props) => {
             color={
               sortingMethod === SortingType.HighestPrice ? 'white' : 'gray.600'
             }
-            onClick={() => setSortingMethod(SortingType.HighestPrice)}
+            onClick={() => {
+              setSortingMethod(SortingType.HighestPrice);
+              sortByHighestPrice();
+            }}
           >
             Highest Price
           </Button>
         </HStack>
-        <IconButton
-          icon={<Image src='assets/icons/arrow-right.svg' />}
-          aria-label='search-options'
-          justifySelf='end'
-          variant='unstyled'
-        />
+        <HStack align='center' justify='center' spacing={5}>
+          <IconButton
+            icon={<Image src='assets/icons/arrow-left.svg' />}
+            display={showLeftArrow ? 'inherit' : 'none'}
+            aria-label='search-options-left'
+            justifySelf='end'
+            variant='unstyled'
+            pt={2}
+            _focus={{ border: 'none' }}
+            onClick={() => {
+              if (range.start - 16 < 0) return;
+              setShowLeftArrow(false);
+              setRange({ start: range.start - 16, end: range.end - 16 });
+            }}
+          />
+          <IconButton
+            icon={<Image src='assets/icons/arrow-right.svg' />}
+            aria-label='search-options'
+            justifySelf='end'
+            variant='unstyled'
+            _focus={{ border: 'none' }}
+            onClick={() => {
+              if (range.start + 16 >= products.length) return;
+              setShowLeftArrow(true);
+              setRange({ start: range.start + 16, end: range.end + 16 });
+            }}
+          />
+        </HStack>
       </HStack>
       <Divider borderWidth={1.25} />
       {!loading && (
@@ -123,7 +174,7 @@ const ProductList = ({ user, setUser }: Props) => {
           templateRows='repeat(4,1fr)'
           pt={10}
         >
-          {products.slice(0, 16).map((product) => (
+          {products.slice(range.start, range.end).map((product) => (
             <GridItem key={product._id}>
               <ProductDetails product={product} user={user} setUser={setUser} />
             </GridItem>
@@ -133,14 +184,36 @@ const ProductList = ({ user, setUser }: Props) => {
 
       <HStack w='100%' justify='space-between' py={12} pb={0}>
         <Text fontSize='xl' color='gray.700'>
-          16 of 32 products
+          16 of {products.length} products
         </Text>
-        <IconButton
-          aria-label='bottom-next-products'
-          icon={<Image src='assets/icons/arrow-right.svg' />}
-          justifySelf='end'
-          variant='unstyled'
-        />
+        <HStack align='center' justify='center' spacing={5}>
+          <IconButton
+            icon={<Image src='assets/icons/arrow-left.svg' />}
+            display={showLeftArrow ? 'inherit' : 'none'}
+            aria-label='search-options-left'
+            justifySelf='end'
+            variant='unstyled'
+            pt={2}
+            _focus={{ border: 'none' }}
+            onClick={() => {
+              if (range.start - 16 < 0) return;
+              setShowLeftArrow(false);
+              setRange({ start: range.start - 16, end: range.end - 16 });
+            }}
+          />
+          <IconButton
+            icon={<Image src='assets/icons/arrow-right.svg' />}
+            aria-label='search-options'
+            justifySelf='end'
+            variant='unstyled'
+            _focus={{ border: 'none' }}
+            onClick={() => {
+              if (range.start + 16 >= products.length) return;
+              setShowLeftArrow(true);
+              setRange({ start: range.start + 16, end: range.end + 16 });
+            }}
+          />
+        </HStack>
       </HStack>
       <Divider borderWidth={1} />
     </VStack>
